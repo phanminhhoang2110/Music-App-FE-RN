0e272cea152d2edc9f1696d83613d2dc581099f0
Phan Minh Hoang <phanminhhoang2110@gmail.com>
2022-05-17 15:58:32 +0700

    add redux

--- index.js
+++ index.js
@@ -3,11 +3,21 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import React from 'react';
+import { Provider } from 'react-redux';
 import {name as appName} from './app.json';
 import './src/designSystem/AssetIcon'
 import './src/designSystem/ColorStyle'
 import './src/designSystem/TextStyle'
+import {store} from './src/redux/store'
 import RootStack from './src/screens/nav/RootStack';
 
-AppRegistry.registerComponent(appName, () => RootStack);
+
+const App = () => {
+    return (
+        <Provider store={store}>
+            <RootStack></RootStack>
+        </Provider>
+    );
+}
+AppRegistry.registerComponent(appName, () => App);
